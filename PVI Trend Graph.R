29a86e1f4a3b40682d9b0aa7b6fa6ebc38b88acc
J.Ibarguen <pvi.specialist@gvc-italia.org>
2018-05-11 13:53:41 +0800

    Point Data Labels
    Limit point data labels to its corresponding quadrant

--- PVI Trend Graph.R
+++ PVI Trend Graph.R
@@ -38,13 +38,18 @@ PVI_totals <- rbind(PVI_totals, a1)
 rm(a, a1)
 
 #PVI TREND GRAPH: COLOURED BY GOVERNORATE
+#Definition of objects
+  lim <- unique(PVI_totals$Governorate)
+  cols <- c("firebrick3","darkkhaki","olivedrab3","gray35","dodgerblue4","darkgoldenrod4","darkgoldenrod2","sienna1","lightskyblue4","darkolivegreen") #Define colours ff manual colors want to be set for Governorate category
+  MA<- PVI_totals[PVI_totals$change < 0 & PVI_totals$endline < 50,]
+  CU<- PVI_totals[PVI_totals$change < 0 & PVI_totals$endline > 50,]
+  LM<- PVI_totals[PVI_totals$change > 0 & PVI_totals$endline < 50,]
+  FB<- PVI_totals[PVI_totals$change > 0 & PVI_totals$endline > 50,]
 dev.new()
 #Aesthetics definition
 splot <- ggplot() + geom_pointrange(aes(x = change,y = endline,ymin = endline,ymax = baseline,colour = Governorate,group= Governorate,shape = factor(Geo.Level),size=factor(Geo.Level)),data=PVI_totals,linetype = 3,na.rm = FALSE)
-#Plot content formatting
-  lim <- unique(PVI_totals$Governorate)
-  cols <- c("firebrick3","darkkhaki","olivedrab3","gray35","dodgerblue4","darkgoldenrod4","darkgoldenrod2","sienna1","lightskyblue4","darkolivegreen") #Define colours ff manual colors want to be set for Governorate category
-  splot+
+#Data points formating
+splot+
   #scale_colour_hue(limits= lim, h=c(0,360)+15,c=100,l=40,h.start=1,direction= 1)+ #If hue colors want to be set for Governorate category. diable object "col" and F(x) "scale_colour_manual"
   scale_colour_manual(limits= lim, values= cols)+
   scale_shape_manual(values=c(8, 19))+
@@ -54,14 +59,36 @@ splot <- ggplot() + geom_pointrange(aes(x = change,y = endline,ymin = endline,ym
   scale_y_continuous(name="PVI Endline Value", limits=c(min(PVI_totals$endline,PVI_totals$baseline)-2,max(PVI_totals$endline,PVI_totals$baseline)+2),
                      breaks=seq(round(min(PVI_totals$endline,PVI_totals$baseline),-1),round(max(PVI_totals$endline,PVI_totals$baseline),+1),5),expand=c(0.0, 0))+
 #Labelling
-  labs(title= "PVI Trend Analysis for Communities", subtitle= "Categorizing Vulnerability change between baseline and endline", caption="NB: Moving Ahead: reduced vulnerability (Endline-Baseline) and Endline value < 50%; Catching-up: reduced Vul. and Endline ≥ 50%;\nLosing Momentum: increased Vul. and Endline < 50%; Falling Behing: increased Vul. and Endline ≥ 50%")+
+  labs(title= "PVI Trend Analysis for Communities", subtitle= "Categorizing Vulnerability change between baseline and endline", caption="NB: Moving Ahead: reduced vulnerability (Endline-Baseline) and Endline value < 50%; Catching-up: reduced Vul. and Endline ≥ 50%; Losing Momentum: increased Vul. and Endline < 50%; Falling Behing: increased Vul. and Endline ≥ 50%;\nLabelled communities represent the outlying 2.5% of all communities")+
   geom_text_repel(mapping=aes(x = change,y = endline,label = Community),
-       data=subset(PVI_totals, change < quantile(PVI_totals$change, probs = 0.05) | change > quantile(PVI_totals$change, probs = 0.95) |
-       endline < quantile(PVI_totals$endline, probs = 0.05) | endline > quantile(PVI_totals$endline, probs = 0.95)), #change values here to set the range of points to be labelled
-       max.iter=5000,force=1,
-       colour = "Grey10",size = 3,
-       segment.color= "black",segment.size=0.5,segment.alpha=0.25,
-       nudge_x = 0,nudge_y = 1,box.padding=2,point.padding=0.5)+
+      data=subset(MA, change < quantile(PVI_totals$change, probs = 0.025) | endline < quantile(PVI_totals$endline, probs = 0.025)),
+      max.iter=3000,force=1,
+      colour = "Grey10",size = 3,
+      segment.color= "black",segment.size=0.5,segment.alpha=0.25,
+      nudge_x = 0,nudge_y = 1,box.padding=2,point.padding=1,
+      xlim=c(NA,-10),ylim=c(NA,50))+
+  geom_text_repel(mapping=aes(x = change,y = endline,label = Community),
+      data=subset(CU,change < quantile(PVI_totals$change, probs = 0.025) | endline > quantile(PVI_totals$endline, probs = 0.975)),
+      max.iter=3000,force=1,
+      colour = "Grey10",size = 3,
+      segment.color= "black",segment.size=0.5,segment.alpha=0.25,
+      nudge_x = 0,nudge_y = 1,box.padding=2,point.padding=1,
+      xlim=c(NA,-17),ylim=c(50,NA))+
+  geom_text_repel(mapping=aes(x = change,y = endline,label = Community),
+      data=subset(LM,change > quantile(PVI_totals$change, probs = 0.975) | endline < quantile(PVI_totals$endline, probs = 0.025)),
+      max.iter=3000,force=1,
+      colour = "Grey10",size = 3,
+      segment.color= "black",segment.size=0.5,segment.alpha=0.25,
+      nudge_x = 0,nudge_y = 1,box.padding=2,point.padding=1,
+      xlim=c(4,NA),ylim=c(NA,45))+
+  geom_text_repel(mapping=aes(x = change,y = endline,label = Community),
+      data=subset(FB,change > quantile(PVI_totals$change, probs = 0.975) | endline > quantile(PVI_totals$endline, probs = 0.975)),
+      max.iter=3000,force=1,
+      colour = "Grey10",size = 3,
+      segment.color= "black",segment.size=0.5,segment.alpha=0.25,
+      nudge_x = 0,nudge_y = 1,box.padding=2,point.padding=1,
+      xlim=c(5,NA),ylim=c(50,NA))+
+
 #Plot background
   #Quadrant lines
   geom_hline(data=PVI_totals,yintercept = 50.0,colour = "grey13",linetype = 2) +
@@ -80,7 +107,7 @@ splot <- ggplot() + geom_pointrange(aes(x = change,y = endline,ymin = endline,ym
   theme(line = element_blank(),
         plot.title = element_text(face = 'bold',size = 15.0),
         plot.subtitle=element_text(size=12,face="plain"),
-        plot.caption=element_text(hjust = 0,face="plain"),
+        plot.caption=element_text(hjust = 0,face="plain",size=9),
         axis.title = element_text(face = 'bold',colour = "black", size= 12),
         axis.text = element_text(face = 'bold',colour = "black", size=9),
         axis.ticks = element_line(),
